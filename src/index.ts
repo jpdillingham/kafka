906c9cbc9ae2b9030665802e26526b15711186ff
JP Dillingham <jp@dillingham.me>
2025-06-29 18:58:05 -0500

    make it work

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ const producer = new Kafka().producer({
   'bootstrap.servers': 'localhost:9092,localhost:9093'
 });
 
-// var consumer = new Kafka().consumer({
-//   'bootstrap.servers': 'localhost:9092,localhost:9093',
-//   'group.id': 'kafka-test',
-// });
+var consumer = new Kafka().consumer({
+  'bootstrap.servers': 'localhost:9092,localhost:9093',
+  'group.id': 'kafka-test',
+});
 
 const app = express();
 app.use(express.json());
@@ -22,44 +22,45 @@ app.get('/', (req, res) => {
 
 app.post('/produce', async (req, res) => {
   try {
-    // const deliveryReports = await producer.send({
-    //     topic: 'test-topic',
-    //     messages: [
-    //       { value: 'v1', key: 'x' },
-    //     ]
-    // });
+    const deliveryReports = await producer.send({
+        topic: 'default',
+        messages: [
+          { value: 'v1', key: 'x' },
+        ]
+    });
     
-    // res.json({ success: true, message: 'Message sent to Kafka', deliveryReports });
+    res.json({ success: true, message: 'Message sent to Kafka', deliveryReports });
   } catch (error) {
     console.error('Error producing message:', error);
     res.status(500).json({ success: false, error: 'Failed to send message' });
   }
 });
 
-producer.connect();
+await producer.connect();
 
-// consumer.connect().then(() => {
-//   consumer.subscribe({ topic: 'default' });
+await consumer.connect().then(() => {
+  consumer.subscribe({ topic: 'default' });
 
-//   consumer.run({
-//     eachMessage: async ({ topic, partition, message }: any) => {
-//       console.log({
-//         topic,
-//         partition,
-//         headers: message.headers,
-//         offset: message.offset,
-//         key: message.key?.toString(),
-//         value: message.value?.toString(),
-//       });
-//     }
-//   });
-// });
+  consumer.run({
+    eachMessage: async ({ topic, partition, message }: any) => {
+      console.log({
+        topic,
+        partition,
+        headers: message.headers,
+        offset: message.offset,
+        key: message.key?.toString(),
+        value: message.value?.toString(),
+        message,
+      });
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 
 const shutDown = () => {
   producer.disconnect();
-  // consumer.disconnect();
+  consumer.disconnect();
 };
 
 process.on('SIGTERM', shutDown);
